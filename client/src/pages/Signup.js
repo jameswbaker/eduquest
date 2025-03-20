87f2ce8fbe6b54dd473c7bfb416d86c7c7e8319b
kirby1926 <bakj@seas.upenn.edu>
2025-03-20 12:13:14 -0400

    added imports

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,8 @@
-import React, { useState, useEffect, useLocation } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useNavigate } from "react-router-dom";
 import { NavLink } from "react-router-dom";
 import axios from "axios";
+import useLocation from 'react-router-dom';
 // import Cookies from 'js-cookie'; // Import js-cookie
 import './SignUp.css';
 
